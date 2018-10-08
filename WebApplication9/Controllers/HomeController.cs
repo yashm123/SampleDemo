d55476f95201907443c81050789ffc121b3a9fa9
Yash Mamidwar <yashm@kpit.com>
2018-10-08 12:59:06 +0530

    commit

--- WebApplication9/Controllers/HomeController.cs
+++ WebApplication9/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace WebApplication9.Controllers
 
             return View();
         }
+        public ActionResult Contact1()
+        {
+            return View();
+        }
+        
     }
 }
